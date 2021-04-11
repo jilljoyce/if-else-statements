2a91dbdbcb0a469a0796e4dab526bd0e7547df3a
jilljoyce <46486508+jilljoyce@users.noreply.github.com>
2021-04-11 16:56:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const age = 9;
+
+if (age > 18) {
+    console.log("je mag naar binnen!");
+}
+else if (age > 10) {
+    console.log("je bent te jong dus je mag helaas niet naar binnen");
+}
+else {
+    console.log("je bent onder de 10");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("je krijgt toegang tot de ladiesnight!");
+} else {
+    console.log("je krijg helaas geen toegang tot de ladiesnight");
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == 'bob') {
+    console.log("jij bent de bob en mag dus rijden");
+} else {
+    console.log("jij mag niet rijden!");
+}
